e79cf36ae9075d3040d28fff787b83345e6c7c7c
Divyam-112 <divyam212.2012@gmail.com>
2026-06-26 19:12:54 +0530

    fix auth cookie

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -19,7 +19,7 @@ export const googleAuth = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -37,7 +37,7 @@ export const logOut = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     });
     return res.status(200).json({ message: "Logged out successfully." });
   } catch (error) {
